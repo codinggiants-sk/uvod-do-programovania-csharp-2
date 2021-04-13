df05c73351cb61a22f933dd50a077d21c6d65349
Andrej Hosna <hosna.andrej@gmail.com>
2021-04-13 20:12:20 +0100

    Added Lesson5 code from Michal

---
+++ Lekcia 5/WindowsFormsAppScreenSaver/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace WindowsFormsAppScreenSaver
+{
+    partial class Screensaver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 2000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Screensaver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Screensaver";
+            this.Text = "Screensaver";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Screensaver_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Screensaver_KeyDown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Screensaver_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Screensaver_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Lekcia 5/WindowsFormsAppScreenSaver/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppScreenSaver
+{
+    public partial class Screensaver : Form
+    {
+        private int x, y; // variable for storing cursor position
+        private string[] pictures; // array of pictures
+        private int number_of_pictures; // number of loaded pictures
+        private int picture_number; // number of current loaded picture
+
+
+        public Screensaver()
+        {
+            InitializeComponent();
+            BackgroundImageLayout = ImageLayout.Stretch;
+            DoubleBuffered = true;
+            WindowState = FormWindowState.Minimized;
+            FormBorderStyle = FormBorderStyle.None;
+            number_of_pictures = 0;
+            picture_number = 0;
+        }
+
+        private void Screensaver_Load(object sender, EventArgs e)
+        {
+            x = Cursor.Position.X;
+            y = Cursor.Position.Y;
+            string folder = AppDomain.CurrentDomain.BaseDirectory.ToString() + @"..\..\pictures";
+            //string parentFolder = System.IO.Directory.GetParent();
+           
+            //MessageBox.Show(parentFolder);
+             //MessageBox.Show(folder);
+
+            try
+            {
+                pictures = Directory.GetFiles(folder);
+            } catch (Exception)
+            {
+                MessageBox.Show("Error while downloading pictures");
+                Application.Exit();
+            }
+
+            number_of_pictures = pictures.Length;
+
+            if (number_of_pictures == 0)
+            {
+                MessageBox.Show("No pictures in screensaver");
+                Application.Exit();
+            }
+
+            BackgroundImage = Image.FromFile(pictures[picture_number]);
+            Cursor.Hide();
+            WindowState = FormWindowState.Maximized;
+            timer.Start();
+        }
+
+        private void Screensaver_MouseMove(object sender, MouseEventArgs e)
+        {
+            int deltaX = Math.Abs(e.X - x);
+            int deltaY = Math.Abs(e.Y - y);
+
+            if (deltaX > 10 && deltaY > 10)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Screensaver_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Screensaver_MouseClick");
+            Application.Exit();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                // MessageBox.Show(pictures[picture_number++]);
+                BackgroundImage = Image.FromFile(pictures[picture_number++]);
+                if (picture_number >= number_of_pictures)
+                {
+                    picture_number = 0;
+                }
+            } catch (Exception)
+            {
+                MessageBox.Show("Error while downloading a file");
+            }
+        }
+
+        private void Screensaver_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Screensaver_KeyDown");
+            Application.Exit();
+        }
+    }
+}
